4898446aa14ba9fa8d3b0bf70fe9cfb5c4c08001
Sofia <sofia.punal@gmail.com>
2022-12-04 12:59:07 +0100

    Delete Item feature + ctx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import { Fragment, useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todo-context";
+
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prevTodo) => {
-      return prevTodo.concat(newTodo);
-    });
-  };
+  
 
   return (
-    <Fragment>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
-    </Fragment>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,19 @@
-import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
-import classes from './Todos.module.css';
+import classes from "./Todos.module.css";
+import { TodosContext } from "../store/todo-context";
+import { useContext } from "react";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
